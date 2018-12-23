2c60f5fdd5beffd4e09b41fc7aee75fea6417ebc
euna0354 <atdawns@gmail.com>
2018-12-23 17:21:55 +0900

    asp.net mvc 샘플 추가

---
+++ ex03/ImageManagerMvc/ImageManagerMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImageManagerMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
